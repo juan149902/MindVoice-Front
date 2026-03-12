aaa2591460d73fe0a1c7d412beabb348a8a35d74
Isaac Arenas <144003496+Nilosnk@users.noreply.github.com>
2026-03-11 22:56:16 -0700

    Change API base URL to a relative path

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,6 +1,6 @@
 import { InjectionToken } from '@angular/core';
 
-const DEFAULT_API_BASE_URL = 'http://18.223.30.63:5000';
+const DEFAULT_API_BASE_URL = '/api';
 
 export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL', {
   factory: () => DEFAULT_API_BASE_URL,
